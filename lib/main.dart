df4d840724ab16c87ddc371b13000fae310de3eb
Joe02 <Joe02araujo@gmail.com>
2020-07-16 08:47:34 -0300

    Renamed file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:chatbot/pages/home_page.dart';
+import 'package:chatbot/widgets/audio_recognition_chat.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
